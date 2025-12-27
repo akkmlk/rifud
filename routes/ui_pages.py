0cf9edfaf2e04699f89ea142799d8e0accf116eb
celaram <gilangbahari82@gmail.com>
2025-12-27 10:39:06 +0700

    Resolve merge conflict

--- routes/ui_pages.py
+++ routes/ui_pages.py
@@ -25,3 +25,32 @@ def tambah():
 @pages.route('/edit')
 def edit():
     return render_template('resto/edit.html')
+
+@pages.route('/frameset')
+def frameset():
+    return render_template('resto/frameset.html')
+
+@pages.route('/navbar')
+def navbar():
+    return render_template('resto/navbar.html')
+
+@pages.route('/sidebar')
+def sidebar():
+    return render_template('resto/sidebar.html')
+@pages.route('/produk')
+def produk():   
+    return render_template('resto/produk.html')
+@pages.route('/pesanan')
+def pesanan():
+    return render_template('resto/pesanan.html')
+@pages.route('/riwayat_pesanan')
+def riwayat_pesanan():
+    return render_template('resto/riwayat_pesanan.html')
+
+@pages.route('/register-resto')
+def register_resto():
+    return render_template('auth/regist_resto.html')
+
+@pages.route('/home-resto')
+def home_resto():
+    return render_template('resto/home.html')
